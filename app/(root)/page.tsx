395ae836da7a04b618934a01cb2c8fe7687e135f
Timo Heman <timoheman16@gmail.com>
2023-07-14 14:20:39 +0100

    added auth using clerk

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,12 @@
 import { Button } from "@/components/ui/button";
+import { UserButton } from "@clerk/nextjs";
 
-export default function Home() {
+const SetupPage = () => {
 	return (
 		<div className='p-4'>
-			<p>Hello Ecommerce app</p>
+			<UserButton afterSignOutUrl='/' />
 		</div>
 	);
-}
+};
+
+export default SetupPage;
