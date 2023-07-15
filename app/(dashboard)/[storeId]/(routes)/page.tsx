c3ca28a50064c0c1cce254b48f0b6c45a553228a
Timo Heman <timoheman16@gmail.com>
2023-07-15 18:49:41 +0100

    loaded store to dashboard page

--- app/(dashboard)/[storeId]/(routes)/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/page.tsx
@@ -1,5 +1,17 @@
-const DashboardPage = () => {
-	return <div>DashboardPage</div>;
+import prismadb from "@/lib/prismadb";
+
+interface DashboardProps {
+	params: { storeId: string };
+}
+
+const DashboardPage: React.FC<DashboardProps> = async ({
+	params: { storeId },
+}) => {
+	const store = await prismadb.store.findFirst({
+		where: { id: storeId },
+	});
+
+	return <div>Active Store: {store?.name}</div>;
 };
 
 export default DashboardPage;
--- app/(root)/(routes)/page.tsx
+++ app/(root)/(routes)/page.tsx
@@ -14,7 +14,7 @@ const SetupPage = () => {
 		}
 	}, [isOpen, onOpen]);
 
-	return <div className='p-4'>Hello World</div>;
+	return null;
 };
 
 export default SetupPage;
