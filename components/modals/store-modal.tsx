19d94e84424eb83cc3ac0f0d5eb0e7898bfd07c5
Timo Heman <timoheman16@gmail.com>
2023-07-15 16:41:34 +0100

    added post api route for creating stores

--- components/modals/store-modal.tsx
+++ components/modals/store-modal.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+import { useState } from "react";
+import axios from "axios";
 import * as z from "zod";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
@@ -23,6 +25,7 @@ const formSchema = z.object({
 
 export const StoreModal = () => {
 	const storeModal = useStoreModal();
+	const [loading, setLoading] = useState(false);
 
 	const form = useForm<z.infer<typeof formSchema>>({
 		resolver: zodResolver(formSchema),
@@ -32,9 +35,17 @@ export const StoreModal = () => {
 	});
 
 	const onSubmit = async (values: z.infer<typeof formSchema>) => {
-		console.log(values);
+		try {
+			setLoading(true);
 
-		// TODO: Create Store in app
+			const response = await axios.post("/api/stores", values);
+
+			console.log(response.data);
+		} catch (error) {
+			console.log(error);
+		} finally {
+			setLoading(false);
+		}
 	};
 
 	return (
@@ -55,17 +66,27 @@ export const StoreModal = () => {
 									<FormItem>
 										<FormLabel>Name</FormLabel>
 										<FormControl>
-											<Input placeholder='E-Commerce' {...field} />
+											<Input
+												disabled={loading}
+												placeholder='E-Commerce'
+												{...field}
+											/>
 										</FormControl>
 										<FormMessage />
 									</FormItem>
 								)}
 							/>
 							<div className='pt-6 space-x-2 flex items-center justify-end w-full'>
-								<Button variant='outline' onClick={storeModal.onClose}>
+								<Button
+									disabled={loading}
+									variant='outline'
+									onClick={storeModal.onClose}
+								>
 									Cancel
 								</Button>
-								<Button type='submit'>Continue</Button>
+								<Button disabled={loading} type='submit'>
+									Continue
+								</Button>
 							</div>
 						</form>
 					</Form>
